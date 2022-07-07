ceac82e5ce485b1037183d1ac8bfa4146936d71b
Nguyễn Anh Tú <>
2022-07-07 19:49:19 +0700

    create project

---
+++ MoneyManagementApp/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MoneyManagementApp
+//
+//  Created by Nguyễn Anh Tú on 07/07/2022.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ MoneyManagementApp/ViewControllers/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyManagementApp
+//
+//  Created by Nguyễn Anh Tú on 07/07/2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
