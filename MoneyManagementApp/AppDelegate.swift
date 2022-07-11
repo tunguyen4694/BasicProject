dcca0c26004b12bf67fd933f3fef1ea275a42334
Nguyễn Anh Tú <>
2022-07-12 00:38:15 +0700

    add firebase

--- MoneyManagementApp/AppDelegate.swift
+++ MoneyManagementApp/AppDelegate.swift
@@ -7,22 +7,58 @@
 
 import UIKit
 import CoreData
+import FirebaseCore
+import FBSDKCoreKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-var window: UIWindow?
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         
-        let firstVC = FirstViewController()
+        let loginVC = LoginVC()
         
-        window?.rootViewController = firstVC
+        window?.rootViewController = loginVC
         window?.makeKeyAndVisible()
         return true
     }
-
+    
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        )
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else {
+            return
+        }
+        
+        ApplicationDelegate.shared.application(
+            UIApplication.shared,
+            open: url,
+            sourceApplication: nil,
+            annotation: [UIApplication.OpenURLOptionsKey.annotation]
+        )
+    }
+    
+    
 }
 
 // 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
