c28b7c39d6058d29bad6ec4a1da7f41c8b61892a
anhtu <>
2022-07-12 16:09:56 +0700

    add VC in tabbar

---
+++ MoneyManagementApp/Controllers/AddTransactionVC.swift
@@ -0,0 +1,29 @@
+//
+//  AddTransactionVC.swift
+//  MoneyManagementApp
+//
+//  Created by MorHN on 12/07/2022.
+//
+
+import UIKit
+
+class AddTransactionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MoneyManagementApp/Controllers/ReportVC.swift
@@ -0,0 +1,29 @@
+//
+//  ReportVC.swift
+//  MoneyManagementApp
+//
+//  Created by MorHN on 12/07/2022.
+//
+
+import UIKit
+
+class ReportVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
