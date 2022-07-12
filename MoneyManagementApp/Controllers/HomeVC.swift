f20cb45609107b18592ba8e734cf28cb19985bee
anhtu <>
2022-07-12 16:09:16 +0700

    configUI HomeVC

--- MoneyManagementApp/Controllers/HomeVC.swift
+++ MoneyManagementApp/Controllers/HomeVC.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class HomeVC: UIViewController {
-
+    @IBOutlet weak var lblHello: UILabel!
+    @IBOutlet weak var lblUserName: UILabel!
+    @IBOutlet weak var vBalance: UIView!
+    @IBOutlet weak var headerHeightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupUI() {
+        vBalance.layer.cornerRadius = 10
+        vBalance.layer.masksToBounds = false
+        vBalance.layer.shadowColor = UIColor.black.cgColor
+        vBalance.layer.shadowOpacity = 0.1
+        vBalance.layer.shadowOffset = .init(width: 0, height: 2)
     }
-    */
-
+    
 }
