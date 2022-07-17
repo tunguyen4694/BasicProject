69d7eed9e34ee8322561e66207d50275658f7c45
Nguyễn Anh Tú <>
2022-07-17 22:59:41 +0700

    transactionArr sort by date

--- MoneyManagementApp/Views/CustomTabBarController.swift
+++ MoneyManagementApp/Views/CustomTabBarController.swift
@@ -114,7 +114,7 @@ class CustomTabBarController: UITabBarController {
         vc.passData = {[weak self] transaction in
             guard let strongSelf = self, let transaction = transaction else { return }
             strongSelf.controller1.transaction.insert(transaction, at: 0)
-            strongSelf.controller1.transaction.sort(by: { $1.date ?? "" < $0.date ?? "" })
+            strongSelf.controller1.transaction.sort(by: { $1.date ?? Date() < $0.date ?? Date() })
             strongSelf.controller1.tableView.reloadData()
         }
         present(vc, animated: true)
