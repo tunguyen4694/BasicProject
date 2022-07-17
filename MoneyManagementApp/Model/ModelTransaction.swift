54cea925e7186f8ea7b9228e9141c30b4dfeafa5
Nguyễn Anh Tú <>
2022-07-17 23:00:34 +0700

    add model

--- MoneyManagementApp/Model/ModelTransaction.swift
+++ MoneyManagementApp/Model/ModelTransaction.swift
@@ -9,15 +9,19 @@ import Foundation
 import UIKit
 
 struct Transaction {
-    init(image: UIImage? = nil, name: String? = nil, date: String? = nil, amount: String? = nil) {
+    init(image: UIImage? = nil, name: String? = nil, date: Date? = nil, amount: String? = nil, stt: String? = nil, color: UIColor? = nil) {
         self.image = image
         self.name = name
         self.date = date
         self.amount = amount
+        self.stt = stt
+        self.color = color
     }
     
     var image: UIImage?
     var name: String?
-    var date: String?
+    var date: Date?
     var amount: String?
+    var stt: String?
+    var color: UIColor?
 }
