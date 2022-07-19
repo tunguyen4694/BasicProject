c969b2f6f67f2fc7a36d01b641287b5b0424fa01
anhtu <>
2022-07-19 14:02:27 +0700

    setup UI ReportVC

--- MoneyManagementApp/Controllers/Report/ReportVC.swift
+++ MoneyManagementApp/Controllers/Report/ReportVC.swift
@@ -9,21 +9,78 @@ import UIKit
 
 class ReportVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.separatorStyle = .none
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "MonthTBVC", bundle: nil), forCellReuseIdentifier: "MonthTBVC")
+        tableView.register(UINib(nibName: "IncomeExpenseTBVC", bundle: nil), forCellReuseIdentifier: "IncomeExpenseTBVC")
+        
+        if #available(iOS 15.0, *) {        // Xoá line phân cách và padding giữa section và cell
+            tableView.sectionHeaderTopPadding = 0.0
+        }
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension ReportVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let vSection = UIView()
+        vSection.backgroundColor = .separatorColor()
+        return vSection
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 2 {
+            return 1
+        } else {
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MonthTBVC", for: indexPath) as! MonthTBVC
+            cell.selectionStyle = .none
+            
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "IncomeExpenseTBVC", for: indexPath) as! IncomeExpenseTBVC
+            cell.selectionStyle = .none
+            cell.stvContent.layer.borderWidth = 0
+            
+            return cell
+        default:
+            let cell = UITableViewCell(style: .default, reuseIdentifier: "test")
+            cell.textLabel?.text = "Charts will update"
+            
+            cell.selectionStyle = .none
+            
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 1:
+            return 68
+        case 2:
+            return UITableView.automaticDimension
+        default:
+            return UITableView.automaticDimension
+        }
+    }
 }
