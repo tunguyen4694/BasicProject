960a2fca071b8fcfc287bd78f52ac0fa771cfe96
anhtu <>
2022-07-26 17:45:48 +0700

    [Add] file ReportTBVC

---
+++ MoneyManagementApp/Controllers/Report/Cell/ReportTBVC.swift
@@ -0,0 +1,19 @@
+//
+//  ReportTBVC.swift
+//  MoneyManagementApp
+//
+//  Created by MorHN on 26/07/2022.
+//
+
+import UIKit
+
+class ReportTBVC: UITableViewCell {
+
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblAmount: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+}
