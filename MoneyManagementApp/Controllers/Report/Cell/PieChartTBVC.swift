064c1a48faeb24ce38373487ad8c92f1b828f1f8
anhtu <>
2022-07-27 17:18:29 +0700

    [Add] func setPieChart & func setCategoryPieChart in TBVC

--- MoneyManagementApp/Controllers/Report/Cell/PieChartTBVC.swift
+++ MoneyManagementApp/Controllers/Report/Cell/PieChartTBVC.swift
@@ -16,20 +16,64 @@ class PieChartTBVC: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-//        for i in 1...10 {
-//            entries.append(ChartDataEntry(x: Double(1), y: Double(5)))
-//        }
-//
-//        let set = PieChartDataSet(entries: entries)
-//        set.colors = ChartColorTemplates.colorful()
-//
-//        let data = PieChartData(dataSet: set)
-//        chartBar.data = data
-//
+        self.selectionStyle = .none
         chartBar.setExtraOffsets(left: 30, top: 30, right: 30, bottom: 30)
         chartBar.animate(xAxisDuration: 1.4, easingOption: .easeOutBack)
 
+    }
+    
+    func setPieChart(_ totalExpense: Double, _ totalIncome: Double, chartView: PieChartView) {
+        var entries = [PieChartDataEntry]()
+        
+        entries.append(PieChartDataEntry(value: totalExpense, label: "Expense"))
+        entries.append(PieChartDataEntry(value: totalIncome, label: "Income"))
+        
+        let set = PieChartDataSet(entries: entries, label: "")
+        set.colors = ChartColorTemplates.pastel()
+        set.sliceSpace = 2
+        set.selectionShift = 0
+        let data = PieChartData(dataSet: set)
+        chartView.data = data
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.locale = Locale(identifier: "vi_VN")
+        formatter.groupingSeparator = "."
+        data.setValueFormatter(DefaultValueFormatter(formatter: formatter))
+        data.setValueTextColor(.black)
+        
+        chartView.legend.enabled = false
+        //            cell.chartBar.drawHoleEnabled = false
+    }
+    
+    func setCategoryPieChart(_ dictCategory: [String:Int], chartView: PieChartView) {
+        var entries = [PieChartDataEntry]()
+        
+        var amountTotal = 0
+        for (_, value) in dictCategory {
+            amountTotal += value
+        }
+        
+        for (key, value) in dictCategory {
+            entries.append(PieChartDataEntry(value: Double(value*100/amountTotal), label: key))
+        }
+        
+        let set = PieChartDataSet(entries: entries, label: "")
+        set.colors = ChartColorTemplates.pastel()
+        set.sliceSpace = 2
+        set.xValuePosition = .outsideSlice
+        set.selectionShift = 0
+        let data = PieChartData(dataSet: set)
+        chartView.data = data
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .percent
+        formatter.multiplier = 1.0
+        data.setValueFormatter(DefaultValueFormatter(formatter: formatter))
+        data.setValueFont(.regular(ofSize: 12))
+        data.setValueTextColor(.black)
+        chartView.legend.enabled = false
+        //            cell.chartBar.drawHoleEnabled = false
     }
 
 }
